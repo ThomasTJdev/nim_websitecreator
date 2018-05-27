e1a4140fd45eb2bfceecd6377719ffd1097761f1
ThomasTJdev <ttjarloev@gmail.com>
2018-05-27 19:49:48 +0200

    Double :: error in plugin registration

--- plugins/openregistration/routes.nim
+++ plugins/openregistration/routes.nim
@@ -25,7 +25,7 @@
               resp genUsersRegister(db, "Error: You need to verify, that you are not a robot!")
 
 
-        when defined(demo) or c.email == "test@test.com"::
+        when defined(demo) or c.email == "test@test.com":
           redirect("/error/" & encodeUrl("Error: The test user can not add new users"))
 
         if @"name" == "" or @"email" == "":
