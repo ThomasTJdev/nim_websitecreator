a8a4c133de8d0832fb0eafe72f9442a7112c860c
Daniil Yarancev <21169548+Yardanico@users.noreply.github.com>
2018-05-27 11:53:01 +0300

    Simplify files_efs

--- src/resources/files/files_efs.nim
+++ src/resources/files/files_efs.nim
@@ -1,50 +1,32 @@
 import parsecfg, os, strutils, osproc, os
-
-
 import ../utils/logging
 
 
 let appDir = getAppDir()
-
 let dict = loadConfig("config/config.cfg")
 
+const section = when defined(dev): "storagedev" else: "storage"
+let tempDir = dict.getSectionValue("Storage", section)
 
 # Create folders
 dbg("INFO", "Checking that required 'files' folders exists")
-discard existsOrCreateDir("files")
-discard existsOrCreateDir("files/efs")
-discard existsOrCreateDir("fileslocal")
+createDir("files")
+createDir("files/efs")
+createDir("fileslocal")
 
-
+createDir(tempDir)
+createDir(tempDir & "/tmp")
+createDir(tempDir & "/files")
+createDir(tempDir & "/files/private")
+createDir(tempDir & "/files/public")
+createDir(tempDir & "/users")
 
 # Storage settings
 when defined(dev):
-  var temp = dict.getSectionValue("Storage","storagedev")
+  dbg("INFO", "Symlinking " & tempDir & " to files/efs")
+  discard execCmd("ln -sf " & appDir & "/" & tempDir & "/* " & appDir & "/files/efs/")
+else:
+  dbg("INFO", "Symlinking " & tempDir & " to files/efs")
+  discard execCmd("ln -sf " & tempDir & "/* " & appDir & "/files/efs/")
 
-  discard existsOrCreateDir(temp)
-  discard existsOrCreateDir(temp & "/tmp")
-  discard existsOrCreateDir(temp & "/files")
-  discard existsOrCreateDir(temp & "/files/private")
-  discard existsOrCreateDir(temp & "/files/public")
-  discard existsOrCreateDir(temp & "/users")
-  
-  dbg("INFO", "Symlinking " & temp & " to files/efs")
-  discard execCmd("ln -sf " & appDir & "/" & temp & "/* " & appDir & "/files/efs/")
-
-
-when not defined(dev):
-  var temp = dict.getSectionValue("Storage","storage")
-
-  discard existsOrCreateDir(temp)
-  discard existsOrCreateDir(temp & "/tmp")
-  discard existsOrCreateDir(temp & "/files")
-  discard existsOrCreateDir(temp & "/files/private")
-  discard existsOrCreateDir(temp & "/files/public")
-  discard existsOrCreateDir(temp & "/users")
-
-  dbg("INFO", "Symlinking " & temp & " to files/efs")
-  discard execCmd("ln -sf " & temp & "/* " & appDir & "/files/efs/")
-  
-
-let storageEFS* = "files/efs"
-
+const storageEFS* = "files/efs"
