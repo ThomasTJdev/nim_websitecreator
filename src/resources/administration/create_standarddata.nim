fe10404fb80dd12547479262711c0cf7a459bdc2
ThomasTJdev <ttjarloev@gmail.com>
2018-06-02 13:01:25 +0200

    Standard theme changed. Updated 'insertdata'

--- src/resources/administration/create_standarddata.nim
+++ src/resources/administration/create_standarddata.nim
@@ -19,7 +19,7 @@ const head* = """
 """
 
 const navbar* = """
-<nav id="navbar" class="navbar navbar-expand-md navbar-light">
+<nav id="navbar" class="navbar navbar-expand-md navbar-dark">
   <div id="navbarInside">
     <a class="navbar-brand" href="/">Nim Website Creator</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
@@ -72,47 +72,165 @@ const footer* = """
 
 
 const about = """
-<h4 class="text-center">
-  Nim Website Creator
-</h4>
-<div class="container-fluid" style="text-align: center;">
-  <div class="row">
-    <div class="col-3 col-md-3">
+<style>
+body .para {
+  height: 100vh;
+}
+</style>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/css3-animate-it/1.0.3/css/animations.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/css3-animate-it/1.0.3/js/css3-animate-it.min.js" defer=""></script>
+<div>
+  <div class="para para_foo" style="background-image: url(/images/par1b.jpeg)">
+    <div class="animatedParent" style="padding-top: 150px;max-width: 200px;margin-left: auto;margin-right: auto;">
+      <div class="animated bounceInRight go" style="background: black;color: white;padding: 20px;border-radius: 10px;text-align: center;">
+        <h1>What</h1>
+        <p>A small and quick website creator.</p>
+      </div>
     </div>
-    <div class="col-6 col-md-6">
-    <dl>
-      <dt>
-        What
-      </dt>
-      <dd>
-        A small and quick website creator.
-      </dd>
-      <dt>
-        Why
-      </dt>
-      <dd>
-        Quick setup of a website with custom configuration
-      </dd>
-      <dt>
-        How
-      </dt>
-      <dd>
-        Clone the GIT repo and you're ready to go!
-      </dd>
-    </dl>
+  </div>
+  <div class="para para_bar" style="background-image: url(/images/par2b.jpeg)">
+    <div class="animatedParent" data-appear-top-offset="-300" style="padding-top: 150px;max-width: 200px;margin-left: auto;margin-right: auto;">
+      <div class="animated fadeInUp" style="background: white;color: black;padding: 20px;border-radius: 10px;text-align: center;">
+        <h1>Why</h1>
+        <p>Quick setup of a website with custom configuration</p><p>
+      </p></div>
     </div>
-    <div class="col-3 col-md-3">
+  </div>
+  <div class="para para_baz" style="background-image: url(/images/par3b.jpeg)">
+    <div class="animatedParent" data-appear-top-offset="-300" style="padding-top: 150px;max-width: 200px;margin-left: auto;margin-right: auto;">
+      <div class="animated growIn" style="background: rgb(88, 234, 94);color: rgb(165, 68, 236);padding: 20px;border-radius: 10px;text-align: center;">
+        <h1>How</h1>
+        <p>Clone the GIT repo and you're ready to go!</p>
+      </div>
     </div>
   </div>
 </div>
 """
 
-const frontpage = """<h1 style="text-align: center; ">Welcome to</h1>
-<h3 style="text-align: center;">Nim Website Creator</h3>
-<p style="text-align: center;"><br></p>
-<p style="text-align: center;">A quick tool to setup a website in no time.</p>
-<p style="text-align: center;">Visit the <a href="https://github.com/ThomasTJdev/nim_websitecreator">Github page</a>&nbsp;for installation instructions or try to <a href="https://nimwc.org/login">login</a>.</p>"""
+const frontpage = """
+<style>
+body .para {
+  height: 70vh;
+}
+.para>div {
+  padding-top:150px;
+  max-width: 600px;
+  margin-left: auto;
+  margin-right: auto;
+}
+@media only screen and (max-width: 768px) {
+ .para>div {
+    padding-top: 50px;
+  }
+}
+</style>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/css3-animate-it/1.0.3/css/animations.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/css3-animate-it/1.0.3/js/css3-animate-it.min.js" defer=""></script>
+<div>
+  <div class="para para_foo" style="background-image: url(/images/par1a.jpeg);">
+    <div class="animatedParent animateOnce">
+      <div class="animated bounceInRight go" style="background: white;color: black;border: 1px solid grey;padding: 20px;border-radius: 10px;text-align: center;">
+        <h1 style="text-align: center; ">Nim Website Creator</h1>
+		<br>
+        <h4 style="text-align: center;">Website in 5 minutes</h4>
+		<p style="text-align: center;">A quick tool to setup a website in no time. Run the program and POW - the website is ready!</p>
+      </div>
+    </div>
+  </div>
+  <div class="para para_bar" style="background-image: url(/images/par2a.jpeg);">
+    <div class="animatedParent animateOnce" data-appear-top-offset="-150">
+      <div class="animated fadeInUp" style="background: white;color: black;border: 1px solid grey;padding: 20px;border-radius: 10px;text-align: center;">
+        <h2>From beginner to expert</h2>
+        <p style="text-align: center;">All the pages can be edited without any HTML (programming) knowledge. Advanced users can edit the header, navbar, footer etc.</p>
+        <br>
+        <h2>Plugins</h2>
+        <p style="text-align: center;">Multiple plugins are available: Schedule mails to all users, backup the database continuously, change themes and more.</p><p>
+      </p></div>
+    </div>
+  </div>
+  <div class="para para_baz" style="background-image: url(/images/par3a.jpeg);">
+    <div class="animatedParent animateOnce" data-appear-top-offset="-150">
+      <div class="animated growIn" style="background: black;color: white;padding: 20px;border-radius: 10px;text-align: center;">
+        <h2>Get started</h2>
+        <p style="text-align: center;">Visit the <a href="https://github.com/ThomasTJdev/nim_websitecreator">Github page</a>&nbsp;for installation instructions or try the <a href="https://nimwc.org/login">test user</a>.</p>
+        <br>
+        <h2>Customize it</h2>
+        <p style="text-align: center;">Nim Website Creator is programmed with Nim-lang. Visit the Github page to learn more.</p>  
+      </div>
+    </div>
+  </div>
+</div>
+"""
 
+
+const blogpost1 = """
+<div style="background: white; color: black; padding: 20px; max-width: 1200px; padding: 20px; border-radius: 10px; margin-left: auto; margin-right: auto; margin-top: 100px;">
+ <h1>Standard blog</h1>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><p><br></p>
+  
+ <div>
+  <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?<br></p>
+ </div>
+</div>
+  
+<div style="height: 50px; background: transparent;"></div>
+  
+<div class="container-fluid" style="max-width: 1200px;">
+  <div class="row">
+    <div class="col-12 col-sm-6">
+      <div style="background: white; color: black; padding: 25px; border-radius: 10px; margin-bottom: 20px;">
+      <h2 style="text-align: center;">UNO</h2>
+      </div>
+    </div>
+    <div class="col-12 col-sm-6">
+      <div style="background: white; color: black; padding: 25px; border-radius: 10px; margin-bottom: 20px;">
+      <h2 style="text-align: center;">DOS</h2>
+      </div>
+    </div>
+  </div>
+</div>
+  
+<div style="height: 50px; background: transparent;"></div>
+  
+<div style="background: white; color: black; padding: 20px; max-width: 1200px; padding: 20px; border-radius: 10px; margin-left: auto; margin-right: auto; margin-bottom: 150px; text-align: right;">
+  <h1 style="text-align: right;">Standard blog</h1>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><p><br></p>
+  
+  <div>
+    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?<br></p>
+  </div>
+</div>
+"""
+
+const blogpost2 = """
+<h1 style="color: white; text-align: center;">Blog post</h1>
+<div class="container-fluid" style="max-width: 1200px; padding-top: 20px;">
+  <div class="row">
+    <div class="col-12 col-sm-4">
+      <div style="background: white; color: black; padding: 25px; border-radius: 10px; margin-bottom: 20px;">
+      <h2 style="text-align: center;">UNO</h2>
+        <p>
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        </p>
+      </div>
+    </div>
+    <div class="col-12 col-sm-4">
+      <div style="background: white; color: black; padding: 25px; border-radius: 10px; text-align: center; margin-bottom: 20px;">
+      <h2 style="text-align: center;">DOS</h2>
+        <img src="/images/avatar.jpg">
+      </div>
+    </div>
+    <div class="col-12 col-sm-4">
+      <div style="background: white; color: black; padding: 25px; border-radius: 10px; margin-bottom: 20px;">
+      <h2 style="text-align: center;">TRES</h2>
+        <p>
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        </p>
+      </div>
+    </div>
+  </div>
+</div>  
+"""
 
 proc standardDataSettings*(db: DbConn) =
   # Settings
@@ -129,7 +247,7 @@ proc standardDataFrontpage*(db: DbConn) =
   echo " - Standard data: Inserting frontpage-data"
   let frontpageExists = getValue(db, sql"SELECT id FROM pages WHERE url = ?", "frontpage")
   if frontpageExists != "":
-    exec(db, sql"DELETE FROM pages WHERE url = ?", "frontpage")
+    exec(db, sql"DELETE FROM pages WHERE id = ?", frontpageExists)
 
   discard insertID(db, sql"INSERT INTO pages (author_id, status, url, name, description, standardhead, standardnavbar, standardfooter) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", "1", "2", "frontpage", "Frontpage", frontpage, "1", "1", "1")
 
@@ -139,9 +257,28 @@ proc standardDataAbout*(db: DbConn) =
   echo " - Standard data: Inserting about-data"
   let aboutExists = getValue(db, sql"SELECT id FROM pages WHERE url = ?", "about")
   if aboutExists != "":
-    exec(db, sql"DELETE FROM pages WHERE url = ?", "about")
+    exec(db, sql"DELETE FROM pages WHERE id = ?", aboutExists)
 
   discard insertID(db, sql"INSERT INTO pages (author_id, status, url, name, description, standardhead, standardnavbar, standardfooter) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", "1", "2", "about", "About", about, "1", "1", "1")
+
+
+proc standardDataBlogpost1*(db: DbConn) =
+  # Blog post
+  echo " - Standard data: Inserting blog post-data"
+  let blogExists = getValue(db, sql"SELECT id FROM blog WHERE url = ?", "standardpost")
+  if blogExists != "":
+    exec(db, sql"DELETE FROM blog WHERE id = ?", blogExists)
+
+  discard insertID(db, sql"INSERT INTO blog (author_id, status, url, name, description, standardhead, standardnavbar, standardfooter) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", "1", "2", "standardpost", "Standard post", blogpost1, "1", "1", "1")
+
+proc standardDataBlogpost2*(db: DbConn) =
+  # Blog post
+  echo " - Standard data: Inserting blog post-data"
+  let blogExists = getValue(db, sql"SELECT id FROM blog WHERE url = ?", "standardpostv2")
+  if blogExists != "":
+    exec(db, sql"DELETE FROM blog WHERE id = ?", blogExists)
+
+  discard insertID(db, sql"INSERT INTO blog (author_id, status, url, name, description, standardhead, standardnavbar, standardfooter) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", "1", "2", "standardpostv2", "Standard post v2", blogpost2, "1", "1", "1")
 
 
 proc createStandardData*(db: DbConn) =
@@ -150,5 +287,7 @@ proc createStandardData*(db: DbConn) =
   standardDataSettings(db)
   standardDataFrontpage(db)
   standardDataAbout(db)
+  standardDataBlogpost1(db)
+  standardDataBlogpost2(db)
   
   
