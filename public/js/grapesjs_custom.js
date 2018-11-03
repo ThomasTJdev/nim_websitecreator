75922d14ab15775a9d89d42b87f8c0bb2d0141b5
ThomasTJdev <ttjarloev@gmail.com>
2018-11-03 05:49:50 +0100

    Comment out bootstrap in GrapesJS as standard

--- public/js/grapesjs_custom.js
+++ public/js/grapesjs_custom.js
@@ -31,8 +31,8 @@ const editor = grapesjs.init({
       '/css/style_custom.css'
     ],
     scripts: [
-      'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js',
-      'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js',
+      //'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js', // Bootstrap
+      //'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js', // Bootstrap
       '/js/js.js',
       '/js/js_custom.js'
     ],
