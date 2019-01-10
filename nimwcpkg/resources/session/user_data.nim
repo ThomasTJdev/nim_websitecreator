0bcd79f57f0b1a7fb6917d6111bc44e7df6434f2
juancarlospaco <juancarlospaco@gmail.com>
2019-01-10 00:27:54 -0300

    Nim Style

--- nimwcpkg/resources/session/user_data.nim
+++ nimwcpkg/resources/session/user_data.nim
@@ -1,22 +1,20 @@
 from jester import Request
 
 
-
 type
-  Rank* = enum
+  Rank* = enum                ## Rank for the User.
     User
     Moderator
     Admin
     Deactivated
     NotLoggedin
 
-type
   Session* = object of RootObj
     loggedIn*: bool
     username*, userpass*, email*: string
-    
+
   TData* = ref object of Session
     req*: Request
-    userid*: string         # User ID
-    timezone*: string       # User timezone
-    rank*: Rank             # User status (rank)
+    userid*: string           ## User ID
+    timezone*: string         ## User timezone
+    rank*: Rank               ## User status (rank)
