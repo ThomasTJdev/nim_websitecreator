9d92b280d386d9fb897446c768d04fccbe63c4b3
juancarlospaco <juancarlospaco@gmail.com>
2019-01-10 01:52:45 -0300

    style,simplify

--- nimwcpkg/resources/email/email_connection.nim
+++ nimwcpkg/resources/email/email_connection.nim
@@ -3,44 +3,41 @@ import asyncdispatch, smtp, strutils, os, htmlparser, asyncnet, parsecfg, times
 # Changing app dir due to, that module is not imported from main module
 setCurrentDir(getAppDir())
 
-var dict = loadConfig(replace(getAppDir(), "/nimwcpkg", "") & "/config/config.cfg")
+const otherHeaders = @[("Content-Type", "text/html; charset=\"UTF-8\"")]
 
-let smtpAddress    = dict.getSectionValue("SMTP","SMTPAddress")
-let smtpPort       = dict.getSectionValue("SMTP","SMTPPort")
-let smtpFrom       = dict.getSectionValue("SMTP","SMTPFrom")
-let smtpUser       = dict.getSectionValue("SMTP","SMTPUser")
-let smtpPassword   = dict.getSectionValue("SMTP","SMTPPassword")
-let adminEmail     = dict.getSectionValue("SMTP","SMTPEmailAdmin")
+let
+  dict = loadConfig(replace(getAppDir(), "/nimwcpkg", "") & "/config/config.cfg")
+  smtpAddress  = dict.getSectionValue("SMTP", "SMTPAddress")
+  smtpPort     = dict.getSectionValue("SMTP", "SMTPPort")
+  smtpFrom     = dict.getSectionValue("SMTP", "SMTPFrom")
+  smtpUser     = dict.getSectionValue("SMTP", "SMTPUser")
+  smtpPassword = dict.getSectionValue("SMTP", "SMTPPassword")
+  adminEmail   = dict.getSectionValue("SMTP", "SMTPEmailAdmin")
 
 
 proc sendMailNow*(subject, message, recipient: string) {.async.} =
   ## Send the email through smtp
-
   when defined(demo):
     echo "Demo is true, email is not send"
-
   when defined(dev) and not defined(devemailon):
     echo "Dev is true, email is not send"
     return
 
-  const otherHeaders = @[("Content-Type", "text/html; charset=\"UTF-8\"")]  
-  
-  # Go Cx Manager
-  var client = newAsyncSmtp(useSsl = true, debug = false)
-  await client.connect(smtpAddress, Port(parseInt(smtpPort)))
-  await client.auth(smtpUser, smtpPassword)
+  let
+    from_addr = smtpFrom
+    toList = @[recipient]
 
-  let from_addr = smtpFrom
-  let toList = @[recipient]
-
-  var headers = otherHeaders
+  var
+    client = newAsyncSmtp(useSsl = true, debug = false)
+    headers = otherHeaders
   headers.add(("From", from_addr))
 
   let encoded = createMessage(subject, message, toList, @[], headers)
 
   try:
+    await client.connect(smtpAddress, Port(parseInt(smtpPort)))
+    await client.auth(smtpUser, smtpPassword)
     await client.sendMail(from_addr, toList, $encoded)
-
   except:
     echo "Error in sending mail: " & recipient
 
@@ -48,38 +45,33 @@ proc sendMailNow*(subject, message, recipient: string) {.async.} =
     echo "Email send"
 
 
-
 proc sendAdminMailNow*(subject, message: string) {.async.} =
-  ## Send the email through smtp
-  ## Clean admin mailing
-
+  ## Send the email through smtp. Clean admin mailing.
   when defined(dev):
     echo "Dev is true, email is not send"
     return
-
   if adminEmail == "":
     echo "No admin email specified"
     return
 
-  let recipient = adminEmail
   let from_addr = adminEmail
-  const otherHeaders = @[("Content-Type", "text/html; charset=\"UTF-8\"")]  
 
-  var client = newAsyncSmtp(useSsl = true, debug = false)
-
-  await client.connect(smtpAddress, Port(parseInt(smtpPort)))
-
-  await client.auth(smtpUser, smtpPassword)
-
-  let toList = @[recipient]
-
-  var headers = otherHeaders
+  var
+    headers = otherHeaders
+    client = newAsyncSmtp(useSsl = true, debug = false)
   headers.add(("From", from_addr))
 
-  let encoded = createMessage("Admin - " & subject, message,
-      toList, @[], headers)
+  let
+    recipient = adminEmail
+    toList = @[recipient]
+    encoded = createMessage("Admin - " & subject, message, toList, @[], headers)
 
-  await client.sendMail(from_addr, toList, $encoded)
+  try:
+    await client.connect(smtpAddress, Port(parseInt(smtpPort)))
+    await client.auth(smtpUser, smtpPassword)
+    await client.sendMail(from_addr, toList, $encoded)
+  except:
+    echo "Error in sending mail: " & recipient
 
   when defined(dev):
     echo "Admin email send"
