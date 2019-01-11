4e824c97142f3b0c2c5156ad07f76f54f66e804c
juancarlospaco <juancarlospaco@gmail.com>
2019-01-11 01:05:45 -0300

    Cleanout, style

--- nimwcpkg/resources/administration/createdb.nim
+++ nimwcpkg/resources/administration/createdb.nim
@@ -6,16 +6,121 @@
 #
 #
 
-import db_sqlite, os, parsecfg, strutils
-
-import ../administration/create_standarddata
+import
+  db_postgres, os, parsecfg, strutils,
+  ../administration/create_standarddata
 
 setCurrentDir(getAppDir().replace("/nimwcpkg", "") & "/")
 
 const
-  TUserName = "varchar(20)"
-  TPassword = "varchar(32)"
-  TEmail = "varchar(30)"
+  personTable = sql"""
+    create table if not exists person(
+      id         integer       primary key,
+      name       varchar(60)   not null,
+      password   varchar(300)  not null,
+      email      varchar(60)   not null,
+      creation   timestamp     not null           default NOW(),
+      modified   timestamp     not null           default NOW(),
+      salt       varchar(128)  not null,
+      status     varchar(30)   not null,
+      timezone   varchar(100),
+      secretUrl  varchar(250),
+      lastOnline timestamp     not null           default NOW()
+    );"""
+
+  sessionTable = sql"""
+    create table if not exists session(
+      id           integer           primary key,
+      ip           inet              not null,
+      key          $#                not null,
+      userid       integer           not null,
+      lastModified timestamp         not null     default NOW(),
+      foreign key (userid) references person(id)
+    );"""
+
+  historyTable = sql"""
+    create table if not exists history(
+      id              integer        primary key,
+      user_id         integer        not null,
+      item_id         integer,
+      element         varchar(100),
+      choice          varchar(100),
+      text            varchar(1000),
+      creation        timestamp      not null     default NOW()
+    );"""
+
+  settingsTable = sql"""
+    create table if not exists settings(
+      id              integer        primary key,
+      analytics       text,
+      head            text,
+      footer          text,
+      navbar          text,
+      title           text,
+      disabled        integer,
+      blogorder       text,
+      blogsort        text
+    );"""
+
+  pagesTable = sql"""
+    create table if not exists pages(
+      id              INTEGER        primary key,
+      author_id       INTEGER        NOT NULL,
+      status          INTEGER        NOT NULL,
+      name            VARCHAR(200)   NOT NULL,
+      url             VARCHAR(200)   NOT NULL     UNIQUE,
+      title           TEXT,
+      metadescription TEXT,
+      metakeywords    TEXT,
+      description     TEXT,
+      head            TEXT,
+      navbar          TEXT,
+      footer          TEXT,
+      standardhead    INTEGER,
+      standardnavbar  INTEGER,
+      standardfooter  INTEGER,
+      tags            VARCHAR(1000),
+      category        VARCHAR(1000),
+      date_start      VARCHAR(100),
+      date_end        VARCHAR(100),
+      views           INTEGER,
+      public          INTEGER,
+      changes         INTEGER,
+      modified        timestamp      not null     default NOW(),
+      creation        timestamp      not null     default NOW(),
+      foreign key (author_id) references person(id)
+    );"""
+
+  blogTable = sql"""
+    create table if not exists blog(
+      id              INTEGER        primary key,
+      author_id       INTEGER        NOT NULL,
+      status          INTEGER        NOT NULL,
+      name            VARCHAR(200)   NOT NULL,
+      url             VARCHAR(200)   NOT NULL     UNIQUE,
+      title           TEXT,
+      metadescription TEXT,
+      metakeywords    TEXT,
+      description     TEXT,
+      head            TEXT,
+      navbar          TEXT,
+      footer          TEXT,
+      standardhead    INTEGER,
+      standardnavbar  INTEGER,
+      standardfooter  INTEGER,
+      tags            VARCHAR(1000),
+      category        VARCHAR(1000),
+      date_start      VARCHAR(100),
+      date_end        VARCHAR(100),
+      views           INTEGER,
+      public          INTEGER,
+      changes         INTEGER,
+      modified        timestamp      not null     default (STRFTIME('%s', 'now')),
+      creation        timestamp      not null     default (STRFTIME('%s', 'now')),
+      foreign key (author_id) references person(id)
+    );
+    """
+
 
 proc generateDB*() =
   echo "Generating database"
@@ -31,147 +136,33 @@ proc generateDB*() =
   if dbexists:
     echo " - Database already exists. Inserting standard tables if they do not exist."
 
-  # Creating folder
-  discard existsOrCreateDir(db_folder)
+  discard existsOrCreateDir(db_folder)  # Creating folder
 
-  # Open DB
-  echo " - Opening database"
-  var db = open(connection=db_host, user=db_user, password=db_pass, database=db_name)
+  echo " - Opening database"  # Open DB
+  var db = db_postgres.open(connection=db_host, user=db_user, password=db_pass, database=db_name)
 
-  if not db.tryExec(sql("""
-  create table if not exists person(
-    id integer primary key,
-    name varchar(60) not null,
-    password varchar(300) not null,
-    email varchar(60) not null,
-    creation timestamp not null default (STRFTIME('%s', 'now')),
-    modified timestamp not null default (STRFTIME('%s', 'now')),
-    salt varbin(128) not null,
-    status varchar(30) not null,
-    timezone VARCHAR(100),
-    secretUrl VARCHAR(250),
-    lastOnline timestamp not null default (STRFTIME('%s', 'now'))
-  );""" % [TUserName, TPassword, TEmail]), []):
+  if not db.tryExec(personTable):
     echo " - Database: person table already exists"
 
-
-
-
-  # -------------------- Session -------------------------------------------------
-
-  if not db.tryExec(sql("""
-  create table if not exists session(
-    id integer primary key,
-    ip inet not null,
-    key $# not null,
-    userid integer not null,
-    lastModified timestamp not null default (STRFTIME('%s', 'now')),
-    foreign key (userid) references person(id)
-  );""" % [TPassword]), []):
+  # Session
+  if not db.tryExec(sessionTable):
     echo " - Database: session table already exists"
 
-
-
-  # ----------------------- History -----------------------------------
-
-  if not db.tryExec(sql"""
-  create table if not exists history(
-    id INTEGER primary key,
-    user_id INTEGER NOT NULL,
-    item_id INTEGER,
-    element VARCHAR(100),
-    choice VARCHAR(100),
-    text VARCHAR(1000),
-    creation timestamp not null default (STRFTIME('%s', 'now'))
-  );""", []):
+  # History
+  if not db.tryExec(historyTable):
     echo " - Database: history table already exists"
 
-
-
-  # ----------------------- Settings -----------------------------------
-
-  if not db.tryExec(sql"""
-  create table if not exists settings(
-    id INTEGER primary key,
-    analytics TEXT,
-    head TEXT,
-    footer TEXT,
-    navbar TEXT,
-    title TEXT,
-    disabled INTEGER,
-    blogorder TEXT,
-    blogsort TEXT
-  );""", []):
+  # Settings
+  if not db.tryExec(settingsTable):
     echo " - Database: settings table already exists"
 
-  # ----------------------- Pages ----------------------------------------------
-
-  if not db.tryExec(sql"""
-  create table if not exists pages(
-    id INTEGER primary key,
-    author_id INTEGER NOT NULL,
-    status INTEGER NOT NULL,
-    name VARCHAR(200) NOT NULL,
-    url VARCHAR(200) NOT NULL UNIQUE,
-    title TEXT,
-    metadescription TEXT,
-    metakeywords TEXT,
-    description TEXT,
-    head TEXT,
-    navbar TEXT,
-    footer TEXT,
-    standardhead INTEGER,
-    standardnavbar INTEGER,
-    standardfooter INTEGER,
-    tags VARCHAR(1000),
-    category VARCHAR(1000),
-    date_start VARCHAR(100) ,
-    date_end VARCHAR(100) ,
-    views INTEGER,
-    public INTEGER,
-    changes INTEGER,
-    modified timestamp not null default (STRFTIME('%s', 'now')),
-    creation timestamp not null default (STRFTIME('%s', 'now')),
-
-    foreign key (author_id) references person(id)
-  );""", []):
+  # Pages
+  if not db.tryExec(pagesTable):
     echo " - Database: pages table already exists"
 
-
-
-  # ----------------------- Blog ----------------------------------------------
-
-  if not db.tryExec(sql"""
-  create table if not exists blog(
-    id INTEGER primary key,
-    author_id INTEGER NOT NULL,
-    status INTEGER NOT NULL,
-    name VARCHAR(200) NOT NULL,
-    url VARCHAR(200) NOT NULL UNIQUE,
-    title TEXT,
-    metadescription TEXT,
-    metakeywords TEXT,
-    description TEXT,
-    head TEXT,
-    navbar TEXT,
-    footer TEXT,
-    standardhead INTEGER,
-    standardnavbar INTEGER,
-    standardfooter INTEGER,
-    tags VARCHAR(1000),
-    category VARCHAR(1000),
-    date_start VARCHAR(100) ,
-    date_end VARCHAR(100) ,
-    views INTEGER,
-    public INTEGER,
-    changes INTEGER,
-    modified timestamp not null default (STRFTIME('%s', 'now')),
-    creation timestamp not null default (STRFTIME('%s', 'now')),
-
-    foreign key (author_id) references person(id)
-  );""", []):
+  # Blog
+  if not db.tryExec(blogTable):
     echo " - Database:blog table already exists"
-
 
   if not dbexists:
     echo "Inserting standard elements"
