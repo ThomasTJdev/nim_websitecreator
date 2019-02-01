073f722c729fe91c5e67298fbbda0a4f0cd5a470
juancarlospaco <juancarlospaco@gmail.com>
2019-02-01 18:40:16 -0300

    Implement new field on user table for 2FA Secret, use a random Secret of 8 digits

--- nimwcpkg/resources/email/email_registration.nim
+++ nimwcpkg/resources/email/email_registration.nim
@@ -4,7 +4,7 @@ import
 
 const
   ActivationMsg = """
-    Hello $1,
+    <h3>Hello $1 </h3>
     <br><br>
     $2 has created a user account for you on $7.
     <br><br>
@@ -14,19 +14,31 @@ const
     $5
     <br><br>
     To login use the details below. On your first login, please navigate to your settings and <b>change your password!</b>
-    <br>
-    Email:    $3
-    <br>
-    Password: $4
-    <br><br>
+    <ul>
+      <li>Email:   <code> $3 </code></li>
+      <li>
+        Password:
+        <input type="text" title="Password" onClick="this.select();document.execCommand('copy')" value="$4" readonly >
+      </li>
+      <li>
+        2 Factor Auth Secret:
+        <input type="number" title="Secret for FreeOTP (TOTP, 6 Digits)" onClick="this.select();document.execCommand('copy')" value="$9" readonly >
+      </li>
+      <li>
+        <a href="https://freeotp.github.io">Get FreeOTP App at https://freeotp.github.io</a>
+      </li>
+    </ul>
     Please do not hesitate to contact us at $8, if you have any questions.
     <br><br>
     Thank you for registering and becoming a part of $6!"""
 
   RegistrationMsg = """
-    Hello $1,
+    <h3>Hello $1 </h3>
     <br><br>
-    We are looking forward to see you at $3! We have sent you an activation email with your password. You just need to click on the link become a part of $3.
+    We are looking forward to see you at $3!
+    We have sent you an activation email with your password.
+    You just need to click on the link become a part of $3.
+    <a href="https://freeotp.github.io">Get FreeOTP App at https://freeotp.github.io</a>
     <br><br>
     Please do not hesitate to contact us at $5, if you have any questions.
     <br><br>
@@ -39,11 +51,11 @@ let
   supportEmail = dict.getSectionValue("SMTP","SMTPEmailSupport")
 
 
-proc sendEmailActivationManual*(email, userName, password, activateUrl, invitorName: string) {.async.} =
+proc sendEmailActivationManual*(email, userName, password, twoFa, activateUrl, invitorName: string) {.async.} =
   ## Send the activation email, when admin added a new user
   let message = ActivationMsg.format(
     userName, invitorName, email, password,
-    (website & activateUrl), title, website, supportEmail)
+    (website & activateUrl), title, website, supportEmail, twoFa)
   await sendMailNow(title & " - Email Confirmation", genEmailMessage(message), email)
 
 
