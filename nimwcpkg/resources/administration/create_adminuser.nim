cda772ad1daf6a07f4f218d50e717b10d95fd142
Juan Carlos <juancarlospaco@gmail.com>
2019-02-22 02:00:38 -0300

    Simplify Salt on Test user
    Salt is just a bunch of 0 because password is test anyways

--- nimwcpkg/resources/administration/create_adminuser.nim
+++ nimwcpkg/resources/administration/create_adminuser.nim
@@ -23,31 +23,32 @@ proc createAdminUser*(db: DbConn, args: seq[string]) =
     elif arg.substr(0, 1) == "e:":
       iEmail = arg.substr(2, arg.len()).strip
 
-  doAssert(iName.len > 3 and iEmail.len > 5 and iPwd.len > 3,
-           "Missing or invalid Name, Email or Password to create Admin user.")
+  doAssert iName.len > 3,  "Missing or invalid Name to create Admin user: " & iName
+  doAssert iEmail.len > 5, "Missing or invalid Email to create Admin user: " & iEmail
+  doAssert iPwd.len > 9,   "Missing or invalid Password to create Admin user."
 
   let
     salt = makeSalt()
     password = makePassword(iPwd, salt)
 
-  discard insertID(db, sql"INSERT INTO person (name, email, password, salt, status, twofa) VALUES (?, ?, ?, ?, ?, ?)", $iName, $iEmail, password, salt, "Admin", base32.encode($rand(10_01.int..89_98.int)))
-
+  discard insertID(db, sql"INSERT INTO person (name, email, password, salt, status) VALUES (?, ?, ?, ?, ?)", $iName, $iEmail, password, salt, "Admin")
   info("Admin added.")
 
 
 proc createTestUser*(db: DbConn) =
   ## Create new admin user.
   info("Checking if any test@test.com exists in DB.")
-  let anyAdmin = getAllRows(db, sql"SELECT id FROM person WHERE email = ?", "test@test.com")
+  const sql_anyAdmin = sql"SELECT id FROM person WHERE email = 'test@test.com'"
+  let anyAdmin = getAllRows(db, sql_anyAdmin)
 
   if anyAdmin.len() < 1:
     info("No test user exists. Creating it!.")
 
-    let
-      salt = makeSalt()
-      password = makePassword("test", salt)
+    const salt = "0".repeat(128)  # Weak Salt for Test user only.
+    let password = makePassword("test", salt)
 
-    discard insertID(db, sql"INSERT INTO person (name, email, password, salt, status, twofa) VALUES (?, ?, ?, ?, ?, ?)", "Testuser", "test@test.com", password, salt, "Moderator", base32.encode($rand(10_01.int..89_98.int)))
+    discard insertID(db, sql("""INSERT INTO person (name, email, password, salt, status)
+      VALUES ('Testuser', 'test@test.com', '', $1, $2, 'Moderator')""".format(password, salt)))
 
     info("Test user added!.")
 
