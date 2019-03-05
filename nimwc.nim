566e36303873c8557ee315db345f639be36e3f12
Juan Carlos <juancarlospaco@gmail.com>
2019-03-05 01:46:52 -0300

    Improve Plugin Skeleton
    Add Links with Info

--- nimwc.nim
+++ nimwc.nim
@@ -245,11 +245,10 @@ proc updateNimwc() =
 
 proc pluginSkeleton() =
   ## Creates the skeleton (folders and files) for a plugin
-
   styledEcho(fgCyan, bgBlack,
     "NimWC: Creating plugin skeleton\nThe plugin will be created inside tmp/")
   let pluginName = normalize(readLineFromStdin("Plugin name: "))
-  echo ""
+  assert pluginName.len > 0, "Plugin Name must not be empty string: " & pluginName
 
   # Create dirs
   discard existsOrCreateDir("tmp")
@@ -257,13 +256,13 @@ proc pluginSkeleton() =
   discard existsOrCreateDir("tmp/" & pluginName & "/public")
 
   # Create files
-  writeFile("tmp/" & pluginName & "/" & pluginName & ".nim", "")
-  writeFile("tmp/" & pluginName & "/routes.nim", "")
-  writeFile("tmp/" & pluginName & "/public/js.js", "")
-  writeFile("tmp/" & pluginName & "/public/style.css", "")
+  writeFile("tmp/" & pluginName & "/" & pluginName & ".nim", "# Code your plugins Backend logic here.\n")
+  writeFile("tmp/" & pluginName & "/routes.nim", "# https://github.com/dom96/jester#routes\n")
+  writeFile("tmp/" & pluginName & "/public/js.js", "/* https://github.com/pragmagic/karax OR Vanilla JavaScript */\n")
+  writeFile("tmp/" & pluginName & "/public/style.css", "/* https://bulma.io/documentation OR https://picturepan2.github.io/spectre OR https://getbootstrap.com */\n")
 
-  if readLineFromStdin("Include optional files (y/N): ").string.strip.toLowerAscii == "y":
-    writeFile("tmp/" & pluginName & "/html.tmpl", "")
+  if readLineFromStdin("\nInclude optional CSS/JS files (y/N): ").string.strip.toLowerAscii == "y":
+    writeFile("tmp/" & pluginName & "/html.tmpl", "<!-- https://nim-lang.org/docs/filters.html -->\n")
     writeFile("tmp/" & pluginName & "/public/js_private.js", "")
     writeFile("tmp/" & pluginName & "/public/style_private.css", "")
 
@@ -273,17 +272,17 @@ proc pluginSkeleton() =
       "name": "$1",
       "foldername": "$2",
       "version": "0.0.1",
-      "url": "",
+      "url": "https://github.com/$3/$2",
       "method": "git",
-      "description": "$3 plugin for Nim Website Creator.",
+      "description": "$2 plugin for Nim Website Creator.",
       "license": "MIT",
       "web": ""
     }
   ]
-  """.format(capitalizeAscii(pluginName), pluginName, pluginName)
+  """.format(capitalizeAscii(pluginName), pluginName, getEnv("USER", "YourUser"))
 
   writeFile("tmp/" & pluginName & "/plugin.json", pluginJson)
-  styledEcho(fgGreen, bgBlack, "NimWC: Created plugin skeleton.")
+  styledEcho(fgGreen, bgBlack, "NimWC: Created plugin skeleton, bye.")
 
 if "help" in args:
   styledEcho(fgGreen, bgBlack, doc)
