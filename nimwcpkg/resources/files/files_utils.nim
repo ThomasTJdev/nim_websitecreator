fcf0e67c4730da5a16301870e26c512121d43fa6
Juan Carlos <juancarlospaco@gmail.com>
2019-03-19 05:27:54 -0300

    Fix #65

--- nimwcpkg/resources/files/files_utils.nim
+++ nimwcpkg/resources/files/files_utils.nim
@@ -1,23 +1,23 @@
-import os, strutils, sequtils, asyncdispatch
+import os
 
 const
-  filesListPrivatePath = "files/efs" & "/files/private/*.*"
-  filesListPublicPath  = "files/efs" & "/files/public/*.*"
+  filesListPrivatePath = "files/efs/files/private/*.*"
+  filesListPublicPath  = "files/efs/files/public/*.*"
 
 
-proc filesListPrivate*(): seq[string] =
+proc filesListPrivate*(): seq[string] {.inline.} =
   ## Get all filenames for project files
   for file in walkFiles(filesListPrivatePath):
     result.add(file)
 
 
-proc filesListPublic*(): seq[string] =
+proc filesListPublic*(): seq[string] {.inline.} =
   ## Get all filenames for project files
   for file in walkFiles(filesListPublicPath):
     result.add(file)
 
 
-proc filesListPublicFolderFiles*(): seq[string] =
+proc filesListPublicFolderFiles*(): seq[string] {.inline.} =
   ## Get all filenames for project files
   for file in walkFiles("public/images/*.*"):
     result.add(file)
