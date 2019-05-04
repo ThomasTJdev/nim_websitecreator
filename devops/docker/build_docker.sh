f46a6c7d762e0953f102d67db942d5f197800043
Manuel Philipp <33608297+manniq31@users.noreply.github.com>
2019-05-04 02:28:06 +0200

    added missing dot

--- devops/docker/build_docker.sh
+++ devops/docker/build_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd base
-docker build -t nimwc_base  # Build the base to always use latest nimwc version
+docker build -t nimwc_base . # Build the base to always use latest nimwc version
 cd ..
 docker build -t nimwc:1.0 . # Build a Docker from the Dockerfile.
