5d09f7563df38fca15afbbc684ac07e6f02a58ab
Juan Carlos <juancarlospaco@gmail.com>
2019-06-04 09:35:01 -0300

    Improve error log mail message, copied from the Log Viewer of NimWC

--- nimwcpkg/resources/email/email_admin.nim
+++ nimwcpkg/resources/email/email_admin.nim
@@ -1,18 +1,27 @@
 import
-  asyncdispatch, smtp, strutils, os, htmlparser, asyncnet, parsecfg,
+  asyncdispatch, smtp, strutils, os, htmlparser, asyncnet, parsecfg, times,
   ../email/email_connection,
   ../email/email_generate_message
 
-const AdminErrorMsg = """
-  Hi Admin
-  <br><br>
-  An error occurred.
-  <br><br>
-  $1
-  <br><br>"""
+
+const adminErrorMsg = """
+  <center>
+    <h1>Error Logs</h1>
+    <p>Hi Admin, an error occurred at $3 </p>
+    <textarea name="logs" id="logs" title="Log Size: $2 Lines." dir="auto" rows=20 readonly autofocus spellcheck style="width:99% !important">
+      $1
+    </textarea>
+    <br>
+    <a title="Copy Logs" onclick="document.querySelector('#logs').select();document.execCommand('copy')">
+      <button>Copy</button>
+    </a>
+    <br>
+  </center>
+  """
 
 
 proc sendEmailAdminError*(msg: string) {.async.} =
   ## Send email - user removed
-  await sendAdminMailNow("Admin: Error occurred",
-                         genEmailMessage(AdminErrorMsg.format(msg)))
+  await sendAdminMailNow(
+    "[NimWC] Admin: Error occurred",
+    genEmailMessage(adminErrorMsg.format(msg, msg.splitLines.len, now())))
