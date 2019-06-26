03b7f45bda34012b2aaa52d7eca0c510978958d1
Juan Carlos <juancarlospaco@gmail.com>
2019-06-26 16:19:19 -0300

    Add some asserts to improve security of code

--- nimwcpkg/resources/password/password_generate.nim
+++ nimwcpkg/resources/password/password_generate.nim
@@ -8,4 +8,6 @@ proc makeSessionKey*(): string {.inline.} =
 
 proc makePassword*(password, salt: string, comparingTo = ""): string {.inline.} =
   ## Creates an MD5 hash by combining password and salt.
+  assert password.len > 0, "password must not be empty string"
+  assert salt.len > 0, "salt must not be empty string"
   hash(getMD5(salt & getMD5(password)), if comparingTo != "": comparingTo else: genSalt(8))
