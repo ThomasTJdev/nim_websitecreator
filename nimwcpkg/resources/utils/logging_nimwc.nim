2802c0d5d6291ade287a9ddd571418c23cb9d665
Juan Carlos <juancarlospaco@gmail.com>
2019-06-27 01:44:31 -0300

    Style, Minor

--- nimwcpkg/resources/utils/logging_nimwc.nim
+++ nimwcpkg/resources/utils/logging_nimwc.nim
@@ -1,4 +1,5 @@
-import os, parsecfg, times, logging, strutils
+import os, parsecfg, times, logging
+from strutils import replace
 import ../email/email_admin
 
 
@@ -16,12 +17,12 @@ let
   fileProd =  dict.getSectionValue("Logging", "logfile")
 
 var
-  console_logger = newConsoleLogger(fmtStr = verboseFmtStr) # Logs to terminal.
-  rolling_file_logger = newRollingFileLogger( # Logs to rotating files.
+  consoleLogger = newConsoleLogger(fmtStr = verboseFmtStr) # Logs to terminal.
+  rollingFileLogger = newRollingFileLogger( # Logs to rotating files.
     when defined(release): fileProd else: fileDebug, fmtStr = verboseFmtStr)
 
-addHandler(console_logger)
-addHandler(rolling_file_logger)
+addHandler(consoleLogger)
+addHandler(rollingFileLogger)
 
 template log2admin*(msg: string) =
   ## Logs the error messages to Admin via mail if ``adminnotify`` is defined.
