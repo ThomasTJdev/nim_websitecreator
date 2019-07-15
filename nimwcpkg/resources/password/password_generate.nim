49eb890ccb9eb77cf6e7870f20d855202d73f4f0
Juan Carlos <juancarlospaco@gmail.com>
2019-07-15 20:34:11 -0300

    Improve contracts

--- nimwcpkg/resources/password/password_generate.nim
+++ nimwcpkg/resources/password/password_generate.nim
@@ -9,6 +9,6 @@ proc makeSessionKey*(): string {.inline.} =
 
 proc makePassword*(password, salt: string, comparingTo = ""): string {.inline.} =
   ## Creates an MD5 hash by combining password and salt.
-  preconditions password.len > 9, salt.len > 100
-  postconditions result.len > password.len + salt.len
-  hash(getMD5(salt & getMD5(password)), if comparingTo != "": comparingTo else: genSalt(8))
+  preconditions password.len > 9, salt.len > 45
+  postconditions result.len > 45
+  result = hash(getMD5(salt & getMD5(password)), if comparingTo != "": comparingTo else: genSalt(8))
