fdf3368873ac9b0228c380417bcbb2e8b1440382
ThomasTJdev <ttjarloev@gmail.com>
2019-08-07 13:39:33 +0200

    Remove pre-cond on pluginPath. It is not passed when status == false (routes.nim line 93)

--- nimwcpkg/resources/utils/plugins.nim
+++ nimwcpkg/resources/utils/plugins.nim
@@ -89,7 +89,7 @@ proc pluginEnableDisable*(pluginPath, pluginName, status: string) =
   ##                       this will enable the plugin (add a line)
   ## @"status" == true  => Plugin is enabled,
   ##                       this will disable the plugin (remove the line)
-  preconditions pluginPath.len > 0, pluginName.len > 0, status in ["false", "true"], existsFile"plugins/plugin_import.txt"
+  preconditions pluginName.len > 0, status in ["false", "true"], existsFile"plugins/plugin_import.txt"
   postconditions existsFile"plugins/plugin_import.txt"
   var newFile = ""
   for line in lines("plugins/plugin_import.txt"):
