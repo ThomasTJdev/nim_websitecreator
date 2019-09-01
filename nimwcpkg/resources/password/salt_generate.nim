676ce3bf574c0df7b2b4e09c96399443298890ca
ThomasTJdev <ttjarloev@gmail.com>
2019-09-01 12:31:06 +0200

    Remove pre-cond from salt check. Salt len can vary to below 45.

--- nimwcpkg/resources/password/salt_generate.nim
+++ nimwcpkg/resources/password/salt_generate.nim
@@ -6,7 +6,6 @@ let useUrandom = urandom.open("/dev/urandom")
 proc makeSalt*(): string =
   ## Generate random salt. Uses cryptographically secure /dev/urandom
   ## on platforms where it is available, and Nim's random module in other cases.
-  postconditions result.len > 45
   if likely(useUrandom):
     var randomBytes: array[0..127, char]
     discard urandom.readBuffer(addr(randomBytes), 128)
