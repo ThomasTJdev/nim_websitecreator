cb36616bf8b4c61c61fb84453e9db15df4233550
Juan Carlos <juancarlospaco@gmail.com>
2019-09-25 18:56:03 -0300

    Move static const very long strings to a dedicated constants file

--- nimwcpkg/resources/email/email_registration.nim
+++ nimwcpkg/resources/email/email_registration.nim
@@ -3,43 +3,9 @@ import asyncdispatch, parsecfg, contra
 from strutils import replace, format
 from os import getAppDir
 
+import ../../constants/constants
 from ../email/email_generate_message import genEmailMessage
 from ../email/email_connection import sendMailNow
-
-
-const
-  activationMsg = """
-    <h3>Hello $1 </h3>
-    <br><br>
-    $2 has created a user account for you on $7.
-    <br><br>
-    As the final step in your registration, we require that you confirm your email
-    via the following link:
-    <br>
-    $5
-    <br><br>
-    To login use the details below. On your first login, please navigate to your settings and <b>change your password!</b>
-    <ul>
-      <li>Email:   <code> $3 </code></li>
-      <li>
-        Password:
-        <input type="text" title="Password" onClick="this.select();document.execCommand('copy')" value="$4" readonly >
-      </li>
-    </ul>
-    Please do not hesitate to contact us at $8, if you have any questions.
-    <br><br>
-    Thank you for registering and becoming a part of $6!"""
-
-  registrationMsg = """
-    <h3>Hello $1 </h3>
-    <br><br>
-    We are looking forward to see you at $3!
-    We have sent you an activation email with your password.
-    You just need to click on the link to become a part of $3.
-    <br><br>
-    Please do not hesitate to contact us at $5, if you have any questions.
-    <br><br>
-    Thank you for registering and becoming a part of $2!"""
 
 
 let
