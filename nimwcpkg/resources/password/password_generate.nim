947ccd1b9c5efc54f09becea32aa04d5382988dc
Juan Carlos <juancarlospaco@gmail.com>
2019-09-25 18:56:11 -0300

    Move static const very long strings to a dedicated constants file

--- nimwcpkg/resources/password/password_generate.nim
+++ nimwcpkg/resources/password/password_generate.nim
@@ -3,12 +3,10 @@ import md5, bcrypt, contra, ../password/salt_generate
 
 template makeSessionKey*(): string =
   ## Creates a random key to be used to authorize a session.
-  postconditions result.len > 0
   bcrypt.hash(makeSalt(), genSalt(8))
 
 
 template makePassword*(password, salt: string, comparingTo = ""): string =
   ## Creates an MD5 hash by combining password and salt.
   preconditions password.len > 3, password.len < 301
-  postconditions result.len > 45
   hash(getMD5(salt & getMD5(password)), if comparingTo != "": comparingTo else: genSalt(8))
