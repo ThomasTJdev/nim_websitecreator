b576b6d2a51e3d403566feaf34a25da8308ce771
Juan Carlos <juancarlospaco@gmail.com>
2019-09-25 18:57:17 -0300

    Move static const very long strings to a dedicated constants file

---
+++ nimwcpkg/constants/integers.nim
@@ -0,0 +1,15 @@
+## Static integer constants, do NOT put any run-time logic here, only consts.
+
+
+const  # nimwcpkg/resources/administration/create_adminuser.nim
+  nameMinLen* = 3
+
+  nameMaxLen* = 60
+
+  emailMinLen* = 5
+
+  emailMaxLen* = 255
+
+  passwordMinLen* = 9
+
+  passwordMaxLen* = 301
