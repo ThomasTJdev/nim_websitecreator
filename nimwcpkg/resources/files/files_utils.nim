0118d247172b287fcbae57146d93c2be465d4b0d
Juan Carlos <juancarlospaco@gmail.com>
2019-09-25 05:09:44 -0300

    "Tiny func inline to template"

--- nimwcpkg/resources/files/files_utils.nim
+++ nimwcpkg/resources/files/files_utils.nim
@@ -2,21 +2,21 @@ import os, contra
 from files_efs import storageEFS
 
 
-proc filesListPrivate*(): seq[string] {.inline.} =
+template filesListPrivate*(): seq[string] =
   ## Get all filenames for project files
   preconditions existsDir(storageEFS / "files/private/")
   for file in walkFiles(storageEFS / "files/private/*.*"):
     result.add(file)
 
 
-proc filesListPublic*(): seq[string] {.inline.} =
+template filesListPublic*(): seq[string] =
   ## Get all filenames for project files
   preconditions existsDir(storageEFS / "files/public/")
   for file in walkFiles(storageEFS / "files/public/*.*"):
     result.add(file)
 
 
-proc filesListPublicFolderFiles*(): seq[string] {.inline.} =
+template filesListPublicFolderFiles*(): seq[string] =
   ## Get all filenames for project files
   preconditions existsDir("public/images/")
   for file in walkFiles("public/images/*.*"):
