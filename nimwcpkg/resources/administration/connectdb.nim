c266929f6eeadb5d2ee30d9db967a0e645247817
Juan Carlos <juancarlospaco@gmail.com>
2019-09-26 01:24:55 -0300

    Simplify Reading configs

--- nimwcpkg/resources/administration/connectdb.nim
+++ nimwcpkg/resources/administration/connectdb.nim
@@ -1,4 +1,6 @@
-import os, parsecfg, strutils
+import os, parsecfg, tables
+
+import ../utils/configs
 
 when defined(postgres): import db_postgres
 else:                   import db_sqlite
@@ -8,20 +10,20 @@ template connectDb*(configFile = "config/config.cfg"): untyped =
   ## Connect the Database and injects a ``db`` variable with the ``DbConn``.
   assert configFile.len > 0, "configFile must not be empty string."
   let
-    dict = loadConfig(configFile)
-    db_host = dict.getSectionValue("Database", "host").strip
+    dict = getConfig(configFile, "Database")
+    db_host = dict["host"]
   assert db_host.len > 2, "db_host must not be empty string."
   when defined(postgres):
     let
-      db_user = dict.getSectionValue("Database", "user").strip
-      db_pass = dict.getSectionValue("Database", "pass").strip
-      db_name = dict.getSectionValue("Database", "name").strip
+      db_user = dict["user"]
+      db_pass = dict["pass"]
+      db_name = dict["name"]
     assert db_user.len > 0, "db_user must not be empty string."
     assert db_pass.len > 0, "db_pass must not be empty string."
     assert db_name.len > 0, "db_name must not be empty string."
     var db {.inject.} = db_postgres.open(connection=db_host, user=db_user, password=db_pass, database=db_name)
   else:
-    let db_folder = dict.getSectionValue("Database", "folder").strip
+    let db_folder = dict["folder"]
     assert db_folder.len > 0, "db_folder must not be empty string."
     discard existsOrCreateDir(db_folder)  # Creating folder
     var db {.inject.} = db_sqlite.open(db_host, "", "", "")
