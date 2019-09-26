c20a66d241bf00cf38d0bed8c805a6607b71974e
Juan Carlos <juancarlospaco@gmail.com>
2019-09-25 21:49:31 -0300

    Simplify connectdb

--- nimwcpkg/resources/administration/connectdb.nim
+++ nimwcpkg/resources/administration/connectdb.nim
@@ -1,4 +1,4 @@
-import os, logging, parsecfg, strutils
+import os, parsecfg, strutils
 
 when defined(postgres): import db_postgres
 else:                   import db_sqlite
@@ -9,26 +9,19 @@ template connectDb*(configFile = "config/config.cfg"): untyped =
   assert configFile.len > 0, "configFile must not be empty string."
   let
     dict = loadConfig(configFile)
-    db_user {.used.} = dict.getSectionValue("Database", "user").strip
-    db_pass {.used.} = dict.getSectionValue("Database", "pass").strip
-    db_name {.used.} = dict.getSectionValue("Database", "name").strip
     db_host = dict.getSectionValue("Database", "host").strip
-    db_folder = dict.getSectionValue("Database", "folder").strip
-    dbexists =
-      when defined(postgres): db_host.len > 2
-      else:                   fileExists(db_host)
-
-  if dbexists: info("Database: Already exists")
-
+  assert db_host.len > 2, "db_host must not be empty string."
   when defined(postgres):
+    let
+      db_user = dict.getSectionValue("Database", "user").strip
+      db_pass = dict.getSectionValue("Database", "pass").strip
+      db_name = dict.getSectionValue("Database", "name").strip
     assert db_user.len > 0, "db_user must not be empty string."
     assert db_pass.len > 0, "db_pass must not be empty string."
     assert db_name.len > 0, "db_name must not be empty string."
-    assert db_host.len > 2, "db_host must not be empty string."
-  else: discard existsOrCreateDir(db_folder)  # Creating folder
-
-  var db {.inject.} =
-    when defined(postgres):
-      db_postgres.open(connection=db_host, user=db_user, password=db_pass, database=db_name)
-    else:
-      db_sqlite.open(db_host, "", "", "")
+    var db {.inject.} = db_postgres.open(connection=db_host, user=db_user, password=db_pass, database=db_name)
+  else:
+    let db_folder = dict.getSectionValue("Database", "folder").strip
+    assert db_folder.len > 0, "db_folder must not be empty string."
+    discard existsOrCreateDir(db_folder)  # Creating folder
+    var db {.inject.} = db_sqlite.open(db_host, "", "", "")
