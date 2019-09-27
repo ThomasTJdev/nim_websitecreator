f13987b1094fd03862e3e4622b9059019beac037
Juan Carlos <juancarlospaco@gmail.com>
2019-09-27 19:39:40 -0300

    Simplify email submodules, style, DRY

--- nimwcpkg/resources/email/email_generate_message.nim
+++ nimwcpkg/resources/email/email_generate_message.nim
@@ -1,18 +1,3 @@
-import contra, tables
-
-from strutils import format, replace
-from os import getAppDir
-
-import ../../constants/constants, ../utils/configs
-
-
-let
-  dict = getConfig(replace(getAppDir(), "/nimwcpkg", "") & "/config/config.cfg", "Server")
-  title = dict["title"]
-  website = dict["website"]
-  mailStyleHeader = mailStyleHeaderMsg.format(website, title)
-  mailStyleFooter = mailStyleFooterMsg.format(website, title)
-
 
 proc genEmailMessage*(msgContent: string): string {.inline.} =
   ## Generate email content
