6d2c7ea7bf55339bb15774dd370d35038b2f89c0
Juan Carlos <juancarlospaco@gmail.com>
2019-09-27 23:59:04 -0300

    Simplify all submodules, less nested structure, more ordering

---
+++ nimwcpkg/plugins/_delete.nim
@@ -0,0 +1,14 @@
+## Do NOT import this file directly, instead import ``plugins.nim``
+
+proc pluginDelete*(pluginFolder: string): bool =
+  ## Delete a Plugin from the filesystem.
+  preconditions pluginFolder.len > 0, existsFile"plugins/plugin_import.txt"
+  for line in lines("plugins/plugin_import.txt"):
+    if line == pluginFolder: return false
+  try:
+    removeDir("plugins/" & pluginFolder)
+    result = true
+  except:
+    result = false
+  finally:
+    echo "Removed Plugin folder and subfolders: ./plugins/" & pluginFolder
