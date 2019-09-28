249b58e925b1423e40625835ed9b1c75d6021e9c
Juan Carlos <juancarlospaco@gmail.com>
2019-09-28 20:45:23 -0300

    Simplify *.nimf template filter files

--- nimwcpkg/nimfs/nimfs.nim
+++ nimwcpkg/nimfs/nimfs.nim
@@ -1,16 +1,23 @@
 
+# Order is important here.
 include
-  "_utils.nimf",             # Utils should be first.
+  "utils/_utils.nimf",              # Utils should be first.
+  "utils/_navbars.nimf",
+  "utils/_editors.nimf",
+  "utils/_profiles.nimf",
+  "utils/_main_new_editors.nimf",
+  "utils/_main_edit_editors.nimf",
+  "utils/_editor_imports.nimf",
 
-  "blogs/_blogs.nimf",       # Blogs
+  "blogs/_blogs.nimf",              # Blogs
   "blogs/_edits.nimf",
   "blogs/_creates.nimf",
 
-  "pages/_pages.nimf",       # Pages
+  "pages/_pages.nimf",              # Pages
   "pages/_edits.nimf",
   "pages/_creates.nimf",
 
-  "_delayredirects.nimf",    # Everthing else
+  "_delayredirects.nimf",           # Everthing else
   "_configs.nimf",
   "_files.nimf",
   "_logs.nimf",
@@ -23,4 +30,4 @@ include
 
 
 when defined(firejail):
-  include "_firejails.nimf"  # Firejail
+  include "_firejails.nimf"         # Firejail
