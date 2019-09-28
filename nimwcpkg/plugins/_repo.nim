121ee7e79866c3f48bf57049f16c967416ddaa21
Juan Carlos <juancarlospaco@gmail.com>
2019-09-27 23:59:31 -0300

    Simplify all submodules, less nested structure, more ordering

---
+++ nimwcpkg/plugins/_repo.nim
@@ -0,0 +1,22 @@
+## Do NOT import this file directly, instead import ``plugins.nim``
+
+proc pluginRepoClone*(): bool =
+  ## Clones (updates) the plugin repo
+  preconditions existsDir(replace(getAppDir(), "/nimwcpkg", "") & "/plugins/")
+  postconditions fileExists("plugins/nimwc_plugins/plugins.json")
+  if unlikely(not("git".findExe.len > 0)): return false
+  let folder = replace(getAppDir(), "/nimwcpkg", "") & "/plugins/"
+  let output = execCmd("git clone " & pluginRepo & " " & folder & pluginRepoName)
+  if output != 0: return false
+  return fileExists("plugins/nimwc_plugins/plugins.json")
+
+proc pluginRepoUpdate*(): bool =
+  ## Clones (updates) the plugin repo
+  preconditions existsDir("plugins" / pluginRepoName)
+  postconditions fileExists("plugins/nimwc_plugins/plugins.json")
+  if unlikely(not("git".findExe.len > 0)): return false
+  let folder = "plugins" / pluginRepoName
+  assert existsDir(folder), "pluginRepoUpdate folder not found (Plugins)"
+  let output = execCmd("git --force -C " & folder & " pull")
+  if output != 0: return false
+  return fileExists("plugins/nimwc_plugins/plugins.json")
