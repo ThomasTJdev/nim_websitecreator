bd0a098d02d4b823850cb1547b5485586b1d911b
Juan Carlos <juancarlospaco@gmail.com>
2019-09-27 23:51:48 -0300

    Simplify all submodules, less nested structure, more ordering

--- nimwcpkg/constants/constants.nim
+++ nimwcpkg/constants/constants.nim
@@ -1,2 +1,2 @@
 ## Just include all constants here, so we keep them separated by topic
-include integers, commands, strings, sqls, htmls  # Order is NOT important here
+include "_integers", "_commands", "_strings", "_sqls", "_htmls"  # Order is NOT important here
