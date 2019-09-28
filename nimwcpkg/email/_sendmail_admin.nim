f9117f8f4467e787d5ba9df847d6f59338f5a424
Juan Carlos <juancarlospaco@gmail.com>
2019-09-27 23:58:06 -0300

    Simplify all submodules, less nested structure, more ordering

---
+++ nimwcpkg/email/_sendmail_admin.nim
@@ -0,0 +1,30 @@
+## Do NOT import this file directly, instead import ``email.nim``
+
+proc sendAdminMailNow*(subject, message: string) {.async.} =
+  ## Send email only to Admin.
+  preconditions subject.len > 0, message.len > 0
+  setCurrentDir(appDir)
+  when defined(dev) and not defined(devemailon):
+    info("Dev is true, email is not sent")
+    return
+  if adminEmail == "":
+    info("No admin email specified")
+    return
+
+  let from_addr = adminEmail
+  var
+    headers = otherHeaders
+    client = newAsyncSmtp(useSsl = true, debug = false)
+  headers.add(("From", from_addr))
+  let
+    recipient = adminEmail
+    toList = @[recipient]
+    encoded = createMessage("Admin - " & subject, message, toList, @[], headers)
+
+  try:
+    await client.connect(smtpAddress, Port(parseInt(smtpPort)))
+    await client.auth(smtpUser, smtpPassword)
+    await client.sendMail(from_addr, toList, $encoded)
+  except:
+    info("Error in sending mail: " & recipient)
+  when defined(dev): info("Admin email sent")
