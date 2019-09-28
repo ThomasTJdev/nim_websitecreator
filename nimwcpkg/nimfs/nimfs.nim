6123ebc7b3e4332f4c144356906317aec38741cd
Juan Carlos <juancarlospaco@gmail.com>
2019-09-28 01:56:32 -0300

    Simplify submodules, everything is Plural because is more future proof

--- nimwcpkg/nimfs/nimfs.nim
+++ nimwcpkg/nimfs/nimfs.nim
@@ -1,22 +1,26 @@
 
 include
-  "utils.nimf",  # Utils should be first.
-  "blogs/blogs.nimf",
-  "blogs/edits.nimf",
-  "blogs/creates.nimf",
-  "delayredirects.nimf",
-  "configs.nimf",
-  "files.nimf",
-  "logs.nimf",
-  "mains.nimf",
-  "pages/pages.nimf",
-  "pages/edits.nimf",
-  "pages/creates.nimf",
-  "plugins.nimf",
-  "statuspages.nimf",
-  "settings.nimf",
-  "sitemaps.nimf",
-  "users.nimf"
+  "_utils.nimf",             # Utils should be first.
+
+  "blogs/_blogs.nimf",       # Blogs
+  "blogs/_edits.nimf",
+  "blogs/_creates.nimf",
+
+  "pages/_pages.nimf",       # Pages
+  "pages/_edits.nimf",
+  "pages/_creates.nimf",
+
+  "_delayredirects.nimf",    # Everthing else
+  "_configs.nimf",
+  "_files.nimf",
+  "_logs.nimf",
+  "_mains.nimf",
+  "_plugins.nimf",
+  "_statuspages.nimf",
+  "_settings.nimf",
+  "_sitemaps.nimf",
+  "_users.nimf"
 
 
-when defined(firejail): include "firejails.nimf"
+when defined(firejail):
+  include "_firejails.nimf"  # Firejail
