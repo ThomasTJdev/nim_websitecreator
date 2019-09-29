d7f8c875d53a17cba013753914a2fbf543f1dfa1
Juan Carlos <juancarlospaco@gmail.com>
2019-09-29 20:23:05 -0300

    Simplify *.nimf template filter files

--- nimwcpkg/nimfs/nimfs.nim
+++ nimwcpkg/nimfs/nimfs.nim
@@ -2,6 +2,8 @@
 ## This file includes all ``*.nimf`` templates of NimWC Core in proper order,
 ## so we keep them separated by topic, on subfolders and on comfy size per file.
 ## Do not put any run-time logic here.
+## To keep things simple try to follow the logic indentation of the Nim proc,
+## instead of the HTML logic indentation, because HTML wont use it, but Nim does
 
 
 template `%`(idx: untyped): untyped {.used.} = row[idx]  # Used on nimf below.
