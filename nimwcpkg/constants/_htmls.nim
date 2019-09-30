4ef204b2bda349eb079cc995faee2598a8b2912e
Juan Carlos <juancarlospaco@gmail.com>
2019-09-30 04:25:06 -0300

    https://github.com/ThomasTJdev/nim_websitecreator/commit/9533f06ab5984fd878a4aa67f8978c08c9069915#r35287430

--- nimwcpkg/constants/_htmls.nim
+++ nimwcpkg/constants/_htmls.nim
@@ -18,8 +18,8 @@ const
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css">
     <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="/css/style_custom.css">
-    <script src="/js/js.js" crossorigin="anonymous" async defer ></script>
-    <script src="/js/js_custom.js" crossorigin="anonymous" async defer ></script>
+    <script src="/js/js.js" crossorigin="anonymous"></script>
+    <script src="/js/js_custom.js" crossorigin="anonymous"></script>
 
   """.unindent
 
@@ -32,8 +32,8 @@ const
     <link rel="author" href="humans.txt">
     <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="/css/style_custom.css">
-    <script src="/js/js.js" crossorigin="anonymous" async defer ></script>
-    <script src="/js/js_custom.js" crossorigin="anonymous" async defer ></script>
+    <script src="/js/js.js" crossorigin="anonymous"></script>
+    <script src="/js/js_custom.js" crossorigin="anonymous"></script>
 
   """.unindent
 
@@ -51,10 +51,10 @@ const
     <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="/css/style_custom.css">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous" async defer ></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha256-CjSoeELFOcH0/uxWu6mC/Vlrc1AARqbm/jiiImDGV3s=" crossorigin="anonymous" async defer ></script>
-    <script src="/js/js.js" crossorigin="anonymous" async defer ></script>
-    <script src="/js/js_custom.js" crossorigin="anonymous" async defer ></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha256-CjSoeELFOcH0/uxWu6mC/Vlrc1AARqbm/jiiImDGV3s=" crossorigin="anonymous"></script>
+    <script src="/js/js.js" crossorigin="anonymous"></script>
+    <script src="/js/js_custom.js" crossorigin="anonymous"></script>
 
   """.unindent
 
