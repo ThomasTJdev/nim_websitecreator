c07a1ee5012a52ef9141671a382cf365bc1747fd
Juan Carlos <juancarlospaco@gmail.com>
2019-10-06 08:44:44 -0300

    Simplify

--- nimwcpkg/plugins/_repos.nim
+++ nimwcpkg/plugins/_repos.nim
@@ -2,21 +2,16 @@
 
 proc pluginRepoClone*(): bool =
   ## Clones (updates) the plugin repo
-  preconditions existsDir(replace(getAppDir(), "/nimwcpkg", "") & "/plugins/")
-  postconditions fileExists("plugins/nimwc_plugins/plugins.json")
-  if unlikely(not("git".findExe.len > 0)): return false
-  let folder = replace(getAppDir(), "/nimwcpkg", "") / "plugins/"
-  let output = execCmd("git clone " & pluginRepo & " " & folder & pluginRepoName)
-  if output != 0: return false
-  return fileExists("plugins/nimwc_plugins/plugins.json")
+  preconditions pluginRepo.len > 0
+  let cmd = "git clone " & pluginRepo & " " & replace(getAppDir(), "/nimwcpkg", "") / "plugins/nimwc_plugins"
+  when defined(dev): echo cmd
+  execCmdEx(cmd).exitCode == 0
+
 
 proc pluginRepoUpdate*(): bool =
   ## Clones (updates) the plugin repo
   preconditions existsDir("plugins" / pluginRepoName)
   postconditions fileExists("plugins/nimwc_plugins/plugins.json")
-  if unlikely(not("git".findExe.len > 0)): return false
-  let folder = "plugins" / pluginRepoName
-  assert existsDir(folder), "pluginRepoUpdate folder not found (Plugins)"
-  let output = execCmd("git --force -C " & folder & " pull")
-  if output != 0: return false
-  return fileExists("plugins/nimwc_plugins/plugins.json")
+  let cmd = "git -C plugins" / pluginRepoName & " pull"
+  when defined(dev): echo cmd
+  execCmdEx(cmd).exitCode == 0
