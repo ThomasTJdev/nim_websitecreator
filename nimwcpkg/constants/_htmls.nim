0f648cddf6813f0af57957e80cfe1e74d59cd179
ThomasTJdev <ttjarloev@gmail.com>
2020-05-21 07:52:09 +0200

    Final touch on new design

--- nimwcpkg/constants/_htmls.nim
+++ nimwcpkg/constants/_htmls.nim
@@ -703,27 +703,6 @@ const
         <div class="features-wrap">
           <div class="feature is-revealing">
             <div class="feature-inner">
-              <div class="feature-icon">
-                <svg width="64" height="64" xmlns="http://www.w3.org/2000/svg">
-                  <defs>
-                    <linearGradient x1="0%" y1="100%" x2="50%" y2="0%" id="feature-1-a">
-                      <stop stop-color="#F9425F" stop-opacity=".8" offset="0%" />
-                      <stop stop-color="#47A1F9" stop-opacity=".16" offset="100%" />
-                    </linearGradient>
-                    <linearGradient x1="50%" y1="100%" x2="50%" y2="0%" id="feature-1-b">
-                      <stop stop-color="#FDFFDA" offset="0%" />
-                      <stop stop-color="#F97059" stop-opacity=".798" offset="49.935%" />
-                      <stop stop-color="#F9425F" stop-opacity="0" offset="100%" />
-                    </linearGradient>
-                  </defs>
-                  <g fill="none" fill-rule="evenodd">
-                    <path d="M24 48H0V24C0 10.745 10.745 0 24 0h24v24c0 13.255-10.745 24-24 24"
-                      fill="url(#feature-1-a)" />
-                    <path d="M40 64H16V40c0-13.255 10.745-24 24-24h24v24c0 13.255-10.745 24-24 24"
-                      fill="url(#feature-1-b)" />
-                  </g>
-                </svg>
-              </div>
               <h3 class="feature-title mt-24">Self-hosting</h3>
               <p class="text-sm mb-0">By utilizing the powerful language Nim, NimWC is compiled to C code, which is
                 runnable on almost all platforms. This further more makes it incredibly easy to host NimWC on e.g.
@@ -732,27 +711,6 @@ const
           </div>
           <div class="feature is-revealing">
             <div class="feature-inner">
-              <div class="feature-icon">
-                <svg width="68" height="64" xmlns="http://www.w3.org/2000/svg">
-                  <defs>
-                    <linearGradient x1="0%" y1="100%" x2="50%" y2="0%" id="feature-2-a">
-                      <stop stop-color="#F9425F" stop-opacity=".8" offset="0%" />
-                      <stop stop-color="#47A1F9" stop-opacity=".16" offset="100%" />
-                    </linearGradient>
-                    <linearGradient x1="50%" y1="100%" x2="50%" y2="0%" id="feature-2-b">
-                      <stop stop-color="#FDFFDA" offset="0%" />
-                      <stop stop-color="#F97059" stop-opacity=".798" offset="49.935%" />
-                      <stop stop-color="#F9425F" stop-opacity="0" offset="100%" />
-                    </linearGradient>
-                  </defs>
-                  <g fill="none" fill-rule="evenodd">
-                    <path d="M9.941 63.941v-24c0-13.255 10.745-24 24-24h24v24c0 13.255-10.745 24-24 24h-24z"
-                      fill="url(#feature-2-a)" transform="rotate(45 33.941 39.941)" />
-                    <path d="M16 0v24c0 13.255 10.745 24 24 24h24V24C64 10.745 53.255 0 40 0H16z"
-                      fill="url(#feature-2-b)" />
-                  </g>
-                </svg>
-              </div>
               <h3 class="feature-title mt-24">Open source</h3>
               <p class="text-sm mb-0">All of NimWC code is available to the public. Any Nim programmer can contribute
                 with improvements and new features. The codebase is built upon modules, which makes it easy to add new
@@ -761,35 +719,30 @@ const
           </div>
           <div class="feature is-revealing">
             <div class="feature-inner">
-              <div class="feature-icon">
-                <svg width="64" height="64" xmlns="http://www.w3.org/2000/svg">
-                  <defs>
-                    <linearGradient x1="50%" y1="100%" x2="50%" y2="43.901%" id="feature-3-a">
-                      <stop stop-color="#F97059" stop-opacity=".798" offset="0%" />
-                      <stop stop-color="#F9425F" stop-opacity="0" offset="100%" />
-                    </linearGradient>
-                    <linearGradient x1="58.893%" y1="100%" x2="58.893%" y2="18.531%" id="feature-3-b">
-                      <stop stop-color="#F9425F" stop-opacity=".8" offset="0%" />
-                      <stop stop-color="#47A1F9" stop-opacity="0" offset="100%" />
-                    </linearGradient>
-                    <linearGradient x1="50%" y1="100%" x2="50%" y2="0%" id="feature-3-c">
-                      <stop stop-color="#FDFFDA" offset="0%" />
-                      <stop stop-color="#F97059" stop-opacity=".798" offset="49.935%" />
-                      <stop stop-color="#F9425F" stop-opacity="0" offset="100%" />
-                    </linearGradient>
-                  </defs>
-                  <g fill="none" fill-rule="evenodd">
-                    <path fill="url(#feature-3-a)" opacity=".32" d="M0 24h64v40H0z" />
-                    <path fill="url(#feature-3-b)" d="M40 24H24L0 64h64z" />
-                    <path d="M10 10v22c0 12.15 9.85 22 22 22h22V32c0-12.15-9.85-22-22-22H10z" fill="url(#feature-3-c)"
-                      transform="rotate(45 32 32)" />
-                  </g>
-                </svg>
-              </div>
               <h3 class="feature-title mt-24">Wordpress</h3>
               <p class="text-sm mb-0">NimWC is not a replacement for Wordpress but an alternative. NimWC is a
                 self-hosted solution with a minimum of requirements which also ensures, that there are fewer
                 dependencies.</p>
+            </div>
+          </div>
+        </div>
+        <div class="features-wrap">
+          <div class="feature is-revealing">
+            <div class="feature-inner">
+              <h3 class="feature-title mt-24">One-click install</h3>
+              <p class="text-sm mb-0">Setup NimWC in no time. Install using Nim's package manager Nimble, use the Docker template or use the AutoInstall script.</p>
+            </div>
+          </div>
+          <div class="feature is-revealing">
+            <div class="feature-inner">
+              <h3 class="feature-title mt-24">Plugin Store</h3>
+              <p class="text-sm mb-0">Its like an App Store but for features - newsletters, backup, etc. Install plugins within the browser - a feature, a click.</p>
+            </div>
+          </div>
+          <div class="feature is-revealing">
+            <div class="feature-inner">
+              <h3 class="feature-title mt-24">Speed + Security</h3>
+              <p class="text-sm mb-0">Written using the Nim programming language to ensure high performance. Firejail, 2 Factor Authentication and honeypot.</p>
             </div>
           </div>
         </div>
@@ -863,7 +816,7 @@ const
         <div class="newsletter-header text-center">
           <h2 class="section-title mt-0">Interested?</h2>
           <p class="section-paragraph">Get in contact with us, ask a question or checkout the source code - visit us at
-            the Gthub repo.</p>
+            the Github repo.</p>
         </div>
         <a class="button button-primary button-block button-shadow"
           href="https://github.com/ThomasTJdev/nim_websitecreator">Nim Website Creator at Github</a>
