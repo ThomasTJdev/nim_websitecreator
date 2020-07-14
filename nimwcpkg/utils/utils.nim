e2151afa3cb2c1ddd594cb24deef49fde852f3d7
Juan Carlos <JuanCarlospaco@gmail.com>
2020-07-14 16:39:51 -0300

    Clean out

--- nimwcpkg/utils/utils.nim
+++ nimwcpkg/utils/utils.nim
@@ -8,3 +8,16 @@ template hardenedBuild*() =
     {.hint: "Security Hardened mode is enabled.".}
     const hf = "-fstack-protector-all -Wstack-protector --param ssp-buffer-size=4 -pie -fPIE -Wformat -Wformat-security -D_FORTIFY_SOURCE=2 -Wall -Wextra -Wconversion -Wsign-conversion -mindirect-branch=thunk -mfunction-return=thunk -fstack-clash-protection -Wl,-z,relro,-z,now -Wl,-z,noexecstack -fsanitize=signed-integer-overflow -fsanitize-undefined-trap-on-error -fno-common"
     {.passC: hf, passL: hf, assertions: on, checks: on.}
+
+
+template getLibravatarUrl*(email: string, size: range[1..512] = 100, default = "robohash", forceDefault = false,
+    baseUrl = (when defined(ssl): "https://seccdn.libravatar.org/avatar/" else: "http://cdn.libravatar.org/avatar/")): string =
+  ## https://wiki.libravatar.org/api & https://wiki.libravatar.org/features
+  assert email.len > 5, "email must not be empty string"
+  assert email.len < 255, "email must be <255 characters long string"
+  assert '@' in email, "email must be a valid standard email address string"
+  assert baseUrl.len > 5, "baseUrl must be a valid HTTP URL string"
+  (baseUrl & getMD5(email.strip.toLowerAscii) & "?s=" & $size &
+    (if unlikely(default != ""): "&d=" & default else: "") &
+    (when defined(release): "" else: (if unlikely(forceDefault): "&f=y" else: ""))
+  )
