843af926512dfcf9222ef262c8937dab1b27972c
Juan Carlos <JuanCarlospaco@gmail.com>
2020-07-14 14:49:34 -0300

    Clean out

--- nimwcpkg/emails/_senderrors_admins.nim
+++ nimwcpkg/emails/_senderrors_admins.nim
@@ -2,6 +2,6 @@
 
 proc sendEmailAdminError*(msg: string) {.async.} =
   ## Send email - user removed
-  preconditions msg.len > 0
+  assert msg.len > 0
   await sendAdminMailNow("Admin: Error occurred", genEmailMessage(
     adminErrorMsg.format(msg, msg.countLines, now())))
