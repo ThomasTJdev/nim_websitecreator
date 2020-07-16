68b48176c35c4ac32a8b666384acc40aa700bf34
Juan Carlos <JuanCarlospaco@gmail.com>
2020-07-16 16:16:45 -0300

    update

--- nimwcpkg/utils/configs.nim
+++ nimwcpkg/utils/configs.nim
@@ -6,61 +6,63 @@ proc getConfig*(configPath: string, section: ConfigSections): Table[string, stri
   assert configPath.len > 0, "configPath must not be empty string"
   assert configPath[^4..^1] == ".cfg", "configPath must be .cfg file extension"
   assert fileExists(configPath), "configPath file not found"
-  let dict = loadConfig(configPath)
+  let dict = create(Config, sizeOf Config)
+  dict[] = loadConfig(configPath)
   case section
   of cfgDatabase:
     result = {
-      "folder": dict.getSectionValue("Database", "folder"),
-      "host": dict.getSectionValue("Database", "host"),
-      "name": dict.getSectionValue("Database", "name"),
-      "user": dict.getSectionValue("Database", "user"),
-      "pass": dict.getSectionValue("Database", "pass"),
-      "port": dict.getSectionValue("Database", "port"),
+      "folder": dict[].getSectionValue("Database", "folder"),
+      "host": dict[].getSectionValue("Database", "host"),
+      "name": dict[].getSectionValue("Database", "name"),
+      "user": dict[].getSectionValue("Database", "user"),
+      "pass": dict[].getSectionValue("Database", "pass"),
+      "port": dict[].getSectionValue("Database", "port"),
     }.toTable
   of cfgStorage:
     result = {
-      "storagedev": dict.getSectionValue("Storage", "storagedev"),
-      "storage": dict.getSectionValue("Storage", "storage"),
+      "storagedev": dict[].getSectionValue("Storage", "storagedev"),
+      "storage": dict[].getSectionValue("Storage", "storage"),
     }.toTable
   of cfgLogging:
     result = {
-      "logfiledev": dict.getSectionValue("Logging", "logfiledev"),
-      "logfile": dict.getSectionValue("Logging", "logfile"),
+      "logfiledev": dict[].getSectionValue("Logging", "logfiledev"),
+      "logfile": dict[].getSectionValue("Logging", "logfile"),
     }.toTable
   of cfgServer:
     result = {
-      "website": dict.getSectionValue("Server", "website"),
-      "title": dict.getSectionValue("Server", "title"),
-      "url": dict.getSectionValue("Server", "url"),
-      "port": dict.getSectionValue("Server", "port"),
-      "appname": dict.getSectionValue("Server", "appname"),
+      "website": dict[].getSectionValue("Server", "website"),
+      "title": dict[].getSectionValue("Server", "title"),
+      "url": dict[].getSectionValue("Server", "url"),
+      "port": dict[].getSectionValue("Server", "port"),
+      "appname": dict[].getSectionValue("Server", "appname"),
     }.toTable
   of cfgProxy:
     result = {
-      "url": dict.getSectionValue("Proxy", "url"),
-      "path": dict.getSectionValue("Proxy", "path"),
+      "url": dict[].getSectionValue("Proxy", "url"),
+      "path": dict[].getSectionValue("Proxy", "path"),
     }.toTable
   of cfgRecaptcha:
     result = {
-      "Sitekey": dict.getSectionValue("reCAPTCHA", "Sitekey"),
-      "Secretkey": dict.getSectionValue("reCAPTCHA", "Secretkey"),
+      "Sitekey": dict[].getSectionValue("reCAPTCHA", "Sitekey"),
+      "Secretkey": dict[].getSectionValue("reCAPTCHA", "Secretkey"),
     }.toTable
   of cfgSmtp:
     result = {
-      "SMTPAddress": dict.getSectionValue("SMTP", "SMTPAddress"),
-      "SMTPPort": dict.getSectionValue("SMTP", "SMTPPort"),
-      "SMTPUser": dict.getSectionValue("SMTP", "SMTPUser"),
-      "SMTPPassword": dict.getSectionValue("SMTP", "SMTPPassword"),
-      "SMTPFrom": dict.getSectionValue("SMTP", "SMTPFrom"),
-      "SMTPEmailAdmin": dict.getSectionValue("SMTP", "SMTPEmailAdmin"),
-      "SMTPEmailSupport": dict.getSectionValue("SMTP", "SMTPEmailSupport"),
+      "SMTPAddress": dict[].getSectionValue("SMTP", "SMTPAddress"),
+      "SMTPPort": dict[].getSectionValue("SMTP", "SMTPPort"),
+      "SMTPUser": dict[].getSectionValue("SMTP", "SMTPUser"),
+      "SMTPPassword": dict[].getSectionValue("SMTP", "SMTPPassword"),
+      "SMTPFrom": dict[].getSectionValue("SMTP", "SMTPFrom"),
+      "SMTPEmailAdmin": dict[].getSectionValue("SMTP", "SMTPEmailAdmin"),
+      "SMTPEmailSupport": dict[].getSectionValue("SMTP", "SMTPEmailSupport"),
     }.toTable
   of cfgFirejail:
     result = {
-      "folder": dict.getSectionValue("firejail", "folder"),
-      "host": dict.getSectionValue("firejail", "host"),
-      "name": dict.getSectionValue("firejail", "name"),
-      "user": dict.getSectionValue("firejail", "user"),
-      "pass": dict.getSectionValue("firejail", "pass"),
-      "port": dict.getSectionValue("firejail", "port"),
+      "folder": dict[].getSectionValue("firejail", "folder"),
+      "host": dict[].getSectionValue("firejail", "host"),
+      "name": dict[].getSectionValue("firejail", "name"),
+      "user": dict[].getSectionValue("firejail", "user"),
+      "pass": dict[].getSectionValue("firejail", "pass"),
+      "port": dict[].getSectionValue("firejail", "port"),
     }.toTable
+  dealloc dict
