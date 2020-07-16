ad8475b7eae231f34894659d220391311a3a83a9
Juan Carlos <JuanCarlospaco@gmail.com>
2020-07-16 08:29:01 -0300

    Simplify some variables just copying strings around without changing them for no reason

--- nimwcpkg/emails/_registrations.nim
+++ nimwcpkg/emails/_registrations.nim
@@ -3,10 +3,11 @@
 proc sendEmailActivationManual*(email, userName, password, activateUrl, invitorName: string) {.async.} =
   ## Send the activation email, when admin added a new user.
   assert email.len > 5 and userName.len > 0 and password.len > 3 and activateUrl.len > 0 and invitorName.len > 0
-  let message = activationMsg.format(
-    userName, invitorName, email, password,
-    (website & activateUrl), title, website, supportEmail)
-  await sendMailNow(title & " - Email Confirmation", genEmailMessage(message), email)
+  await sendMailNow(title & " - Email Confirmation", genEmailMessage(
+    activationMsg.format(
+      userName, invitorName, email, password, (website & activateUrl), title, website, supportEmail)
+    ), email,
+  )
 
 
 proc sendEmailRegistrationFollowup*(email, userName: string) {.async.} =
